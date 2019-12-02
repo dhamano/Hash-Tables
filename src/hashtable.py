29ae29eb42a86480bc6b9985f344e1f54fb9b109
Dustin <clerin+gitkraken@gmail.com>
2019-12-02 15:47:05 -0800

    WIP need to test

--- src/hashtable.py
+++ src/hashtable.py
@@ -59,7 +59,23 @@ class HashTable:
         Fill this in.
         '''
         index = self._hash_mod(key)
-        self.storage[index] = value
+        el = LinkedPair(key, value)
+        if self.storage[index] is not None:
+            # print(f"self.storage[index] is: {self.storage[index].key}")
+            keep_going = True
+            el_for_eval = self.storage[index]
+            while keep_going:
+                if el_for_eval.next is not None:
+                    # print(f"el_for_eval.key: {el_for_eval.key}")
+                    # print(f"el_for_eval.next.key: {el_for_eval.next.key}")
+                    el_for_eval = el_for_eval.next
+                else:
+                    # print(f"el_for_eval.next is None: {el_for_eval.next}")
+                    el_for_eval.next = el
+                    keep_going = False
+        else:
+            # print(f'no value yet {self.storage[index]}')
+            self.storage[index] = el
 
 
 
@@ -72,7 +88,34 @@ class HashTable:
         Fill this in.
         '''
         index = self._hash_mod(key)
-        self.storage[index] = None
+        if self.storage[index] is None:
+            # print("ERROR: no value to remove")
+            return
+
+        current_el = self.storage[index]
+        if current_el.next is None:
+            self.storage[index] = None
+            return
+
+        keep_going = True
+        stored_key = current_el.key
+        prev_el = self.storage[index]
+        while keep_going:
+            if stored_key == key:
+                if current_el == prev_el:
+                    self.storage[index] = current_el.next
+                    current_el.next = None
+                    keep_going = False
+                elif current_el.next is not None:
+                    prev_el.next = current_el.next
+                    current_el.next = None
+                    keep_going = False
+                else:
+                    stored_key = current_el.next.key
+                    prev_el = current_el
+                    current_el = current_el.next
+                
+
 
 
     def retrieve(self, key):
@@ -84,7 +127,28 @@ class HashTable:
         Fill this in.
         '''
         index = self._hash_mod(key)
-        return self.storage[index]
+        # print("RETRIEVE")
+        if self.storage[index] is None:
+            # print("ERROR: Value DNE")
+            return
+
+        current_el = self.storage[index]
+        if current_el.next is None:
+            # print("only one val")
+            # print(f"current_el.key: {current_el.key} | key: {key}")
+            return current_el.value
+
+        keep_going = True
+        stored_key = current_el.key
+        prev_el = self.storage[index]
+        while keep_going:
+            # print(f"current_el.key: {current_el.key} | key: {key}")
+            if current_el.key != key:
+                # print('not equal')
+                current_el = current_el.next
+            else:
+                # print('equal')
+                return current_el.value
 
 
     def resize(self):
@@ -102,12 +166,13 @@ class HashTable:
 
         self.storage = new_storage
 
-#"""
+"""
 ht = HashTable(3)
 ht.insert("hello",50)
 ht.insert("abacusMAX",2)
 ht.insert("world",50)
 ht.insert("people",2)
+ht.remove("hello")
 
 print(ht.storage)
 
@@ -119,10 +184,10 @@ ht.resize()
 
 print(ht.storage)
 
-print(f'retrieve: {ht.retrieve("hello")}')
+print(f'retrieve: {ht.retrieve("world")}')
 
 #"""
-"""
+# """
 if __name__ == "__main__":
     ht = HashTable(2)
 
