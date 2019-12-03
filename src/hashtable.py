681e76f28be920eac08a7f41a6f90929915937c4
Dustin <clerin+gitkraken@gmail.com>
2019-12-03 13:05:32 -0800

    DJB2 done and linked list finished

--- src/hashtable.py
+++ src/hashtable.py
@@ -65,6 +65,9 @@ class HashTable:
             keep_going = True
             el_for_eval = self.storage[index]
             while keep_going:
+                if el_for_eval.key == key:
+                    el_for_eval.value = value
+                    keep_going = False
                 if el_for_eval.next is not None:
                     # print(f"el_for_eval.key: {el_for_eval.key}")
                     # print(f"el_for_eval.next.key: {el_for_eval.next.key}")
@@ -87,6 +90,7 @@ class HashTable:
 
         Fill this in.
         '''
+        # print("Remove")
         index = self._hash_mod(key)
         if self.storage[index] is None:
             # print("ERROR: no value to remove")
@@ -94,6 +98,7 @@ class HashTable:
 
         current_el = self.storage[index]
         if current_el.next is None:
+            # print(f"current_el.next: {current_el.next}")
             self.storage[index] = None
             return
 
@@ -101,19 +106,31 @@ class HashTable:
         stored_key = current_el.key
         prev_el = self.storage[index]
         while keep_going:
+            # print(f"stored_key: {stored_key} | key: {key}")
             if stored_key == key:
+                # print('EQUAL!')
                 if current_el == prev_el:
+                    # print("FIRST ELEMENT")
+                    # print(f"current_el: {current_el} | prev_el: {prev_el}")
                     self.storage[index] = current_el.next
                     current_el.next = None
                     keep_going = False
                 elif current_el.next is not None:
+                    # print("there is a next")
+                    # print(f"current_el.next: {current_el.next}")
                     prev_el.next = current_el.next
                     current_el.next = None
                     keep_going = False
                 else:
-                    stored_key = current_el.next.key
-                    prev_el = current_el
-                    current_el = current_el.next
+                    # print("ELSE")
+                    prev_el.next = current_el.next
+                    keep_going = False
+                    return
+            else:
+                stored_key = current_el.next.key
+                prev_el = current_el
+                # print(f"stored_key: {stored_key} | prev_el.key: {prev_el.key}")
+                current_el = current_el.next
                 
 
 
@@ -161,33 +178,90 @@ class HashTable:
         self.capacity *= 2
         new_storage = [None] * self.capacity
 
-        for i in range(len(self.storage)):
-            new_storage[i] = self.storage[i]
+        for el in self.storage:
+            index = self._hash_djb2(el.key) % self.capacity
+            new_storage[index] = el
+
+        # for i in range(0,len(self.storage)):
+        #     print(i)
+        #     new_storage[i] = self.storage[i]
+        #     print(f"self.storage[{i}] : {self.storage[i].key, self.storage[i].value} | new_storage[{i}] : {new_storage[i].key, new_storage[i].value}")
 
         self.storage = new_storage
 
+        el_list = []
+        for el in self.storage:
+            if el is not None and el.next is not None:
+                keep_going = True
+                prev_el = el
+                current_el = el
+                while keep_going:
+                    if current_el.next is not None:
+                        el_list.append({"key": current_el.key, "value": current_el.value})
+                        current_el = current_el.next
+                        prev_el.next = None
+                    else:
+                        el_list.append({"key": current_el.key, "value": current_el.value})
+                        current_el = current_el.next
+                        prev_el.next = None
+                        keep_going = False
+                        
+        for i in range(0,len(el_list)):
+            self.insert(el_list[i]["key"], el_list[i]["value"])
+
 """
-ht = HashTable(3)
+ht = HashTable(2)
 ht.insert("hello",50)
 ht.insert("abacusMAX",2)
 ht.insert("world",50)
 ht.insert("people",2)
 ht.remove("hello")
+ht.insert("world",100)
 
 print(ht.storage)
 
 ht.remove("abacusMAX")
 
 print(ht.storage)
+print(f'retrieve: {ht.retrieve("people")}')
+print(f'retrieve: {ht.retrieve("world")}')
 
 ht.resize()
 
 print(ht.storage)
 
+print(f'retrieve: {ht.retrieve("people")}')
 print(f'retrieve: {ht.retrieve("world")}')
 
 #"""
-# """
+"""
+ht = HashTable(8)
+
+ht.insert("key-0", "val-0")
+ht.insert("key-1", "val-1")
+ht.insert("key-2", "val-2")
+ht.insert("key-3", "val-3")
+ht.insert("key-4", "val-4")
+ht.insert("key-5", "val-5")
+ht.insert("key-6", "val-6")
+ht.insert("key-7", "val-7")
+ht.insert("key-8", "val-8")
+ht.insert("key-9", "val-9")
+print(f"insert: {ht.storage}")
+
+ht.remove("key-9")
+ht.remove("key-8")
+ht.remove("key-7")
+ht.remove("key-6")
+ht.remove("key-5")
+ht.remove("key-4")
+ht.remove("key-3")
+ht.remove("key-2")
+ht.remove("key-1")
+ht.remove("key-0")
+print(f"remove: {ht.storage}")
+#"""
+"""
 if __name__ == "__main__":
     ht = HashTable(2)
 
@@ -208,6 +282,7 @@ if __name__ == "__main__":
     new_capacity = len(ht.storage)
 
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    print(f"insert: {ht.storage}")
 
     # Test if data intact after resizing
     print(ht.retrieve("line_1"))
